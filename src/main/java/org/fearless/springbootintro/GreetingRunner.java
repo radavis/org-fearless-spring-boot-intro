73ade1b29f16cbcc95ec999e29240e7f9ece42cd
Richard Davis <rdavis.bacs@gmail.com>
2020-02-04 15:31:43 -0500

    refactor GreetingRunner to use dependency injection

--- src/main/java/org/fearless/springbootintro/GreetingRunner.java
+++ src/main/java/org/fearless/springbootintro/GreetingRunner.java
@@ -1,16 +1,27 @@
 package org.fearless.springbootintro;
 
+import org.fearless.springbootintro.hello.SimpleGreetingService;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GreetingRunner implements ApplicationRunner {
 
+    // @Autowired // can also be delcared, here.
+    // https://www.baeldung.com/spring-autowire
+    private final SimpleGreetingService simpleGreetingService;
+
+    @Autowired
+    public GreetingRunner(SimpleGreetingService simpleGreetingService) {
+        this.simpleGreetingService = simpleGreetingService;
+    }
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        String name = "Spring";
-        String greeting = String.format("Hello, %s!", name);
+        String greeting = simpleGreetingService.greet("Spring");
         System.out.println(greeting);
     }
 }
