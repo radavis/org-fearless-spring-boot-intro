885ba03079825324d8f202a682bf5433607c977e
Richard Davis <rdavis.bacs@gmail.com>
2020-02-04 15:02:40 -0500

    add GreetingRunner

---
+++ README.md
@@ -0,0 +1,23 @@
+# org-fearless-spring-boot-intro
+
+[lesson](https://bclozel.github.io/lectures/spring-boot-lab.html)
+
+[pdf](/introduction-to-spring-boot.pdf)
+
+Generated with:
+
+```
+$ spring init \
+    --dependencies=web,devtools \
+    --groupId=org.fearless \
+    --type=maven-project \
+    spring-boot-intro
+```
+
+## Start it
+
+```
+$ ./mvnw spring-boot:run
+$ open http://localhost:8080
+$ curl http://localhost:8080
+```
---
+++ src/main/java/org/fearless/springbootintro/GreetingRunner.java
@@ -0,0 +1,16 @@
+package org.fearless.springbootintro;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        String name = "Spring";
+        String greeting = String.format("Hello, %s!", name);
+        System.out.println(greeting);
+    }
+}
